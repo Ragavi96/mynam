ec88d76bca98ac1bf07af78b81d1b62ecadc1836
Ragavi96 <30549326+Ragavi96@users.noreply.github.com>
2017-08-12 16:37:37 +0530

    Update and rename .gitignore to noofchar.c

---
+++ .gitignore/noofchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+  char a[100];
+  printf("enter the word");
+  gets(a);
+ while(a!='\0')
+ {
+   int count;
+   count++;
+   }
+   printf("no. of character is%d", a);
+   }
+   
